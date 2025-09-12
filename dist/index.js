ffe677c20cf8041b133cf8ddd8b49f0667a41b94
drissnafii <drissnafi3@gmail.com>
2025-09-12 17:26:24 +0100

    funcitons,sequentials,  and  Unions

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let x = ["hello", 10];
+const w = () => {
+    console.log("hello mr. trump");
+    return;
+    15;
+};
+const v = (x, y) => {
+    return x + y;
+};
+let x = ["driss", 19, false]; // here this is a sequential ! 
+let y = ["salma", false]; // and here is Union of those values =>string|number|boolean 
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-let x = ["hello", 10]
+const w = () => {
+    console.log("hello mr. trump");
+    return ;15;
+};
+
+const v: (x:number, y:number) => number = (x, y) => {
+    return x+y; 
+}
+
+let x:[string, number, boolean] = ["driss", 19, false]; // here this is a sequential ! 
+let y:(string|number|boolean)[] = ["salma", false]; // and here is Union of those values =>string|number|boolean 
