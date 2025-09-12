fb938bd3e1245584a3839f0ac824512b61b8c3fb
drissnafii <drissnafi3@gmail.com>
2025-09-12 18:39:17 +0100

    remove unused functions and types from index.ts and update index.js.map

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,3 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const w = () => {
-    console.log("hello mr. trump");
-    return;
-    15;
-};
-const v = (x, y) => {
-    return x + y;
-};
-let x = ["driss", 19, false]; // here this is a sequential ! 
-let y = ["salma", false]; // and here is Union of those values =>string|number|boolean 
 //# sourceMappingURL=index.js.map
