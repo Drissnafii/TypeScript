26d858faf9ded32c1532270dc65d1b32e995914b
drissnafii <drissnafi3@gmail.com>
2025-09-13 10:02:28 +0100

    uuu

--- src/index.ts
+++ src/index.ts
@@ -16,8 +16,6 @@ type Mytype = {
 }
 
 let wx: Mytype 
-let wc: Mytype 
-
 wx = {
     name: "Driss Nafii",
     age: 19,
@@ -30,4 +28,42 @@ wx = {
 
 console.log(wx.age);
 
+const myNumber: number = wx.age ?? 0;
 
+export enum BookingStatus {
+    initial = "initial",
+    paid = "paid",
+    cancelled = "cancelled"
+}
+
+let bookingStatus:BookingStatus = BookingStatus.initial;
+
+bookingStatus = BookingStatus.paid;
+
+if (bookingStatus == BookingStatus.paid) {
+     
+}
+
+interface ICar {
+    model:string;
+    year:number;
+
+    sayHello: () => void;
+};
+
+class Car implements ICar {
+    model:string;
+    year:number;
+
+    constructor(model:string, year:number) {
+        this.model = model;
+        this.year = year;
+    }
+
+    sayHello = () => {
+        return "Hello";
+    }
+}
+
+const myCar = new Car("BMW", 9999);
+myCar.sayHello;
---
+++ src/types.ts
@@ -0,0 +1,10 @@
+const summation = <T>(nbr1:T,nbr2:T) => {
+    let sum:number
+    // sum = nbr1+nbr2;
+
+    // return sum;
+}
+
+summation<string>("98","false");
+
+summation<number>(199, 0);
