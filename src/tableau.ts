6ef3462a3a6c8da929d2db89f3849c730bb5ce8b
drissnafii <drissnafi3@gmail.com>
2025-09-13 18:15:24 +0100

    refactor array processing logic to separate even and odd numbers

--- src/tableau.ts
+++ src/tableau.ts
@@ -1,12 +1,13 @@
 let arr:any = [232, 884, 23, 5, 7];
-let dkhol = false;
-
+let pair:any = [];
+let impair:any = [];
 for (let i = 0; i < arr.length; i++) {
-    if (arr[i]%2 == 0 && arr[i] < 10) {
-        arr[i] = 0;
-        dkhol = true;
+    if (arr[i]%2 == 0) {
+        pair.push(arr[i]);
+    } else {
+        impair.push(arr[i]);
     }
 }
-if (dkhol) {
-    console.log(arr);
-}else console.log("nothing "); 
+
+console.log(pair);
+console.log(impair);
