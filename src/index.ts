e6aa16955d511a70854e7834dafa0c8140ca2913
drissnafii <drissnafi3@gmail.com>
2025-09-13 08:55:29 +0100

    create types

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+interface Person {
+    name: string,
+    age:number      
+}
+
+let x: Person
+x = {
+    name: "Driss",
+    age: 20,
+}
+
+type Mytype = boolean|string;
+
+let wx: Mytype 
+let wc: Mytype 
+
+wx = true
